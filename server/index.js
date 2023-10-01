fbafb8e0807955da5eea74aedf2d8d3c8946268f
Anton Shimanovich <ashimanovich127@gmail.com>
2023-10-01 18:10:44 +0400

    feat: connect to DB

--- server/index.js
+++ server/index.js
@@ -1,18 +1,22 @@
 const express = require("express");
-// const mongoose = require("mongoose");
+const mongoose = require("mongoose").default;
 const config = require("config");
 
 const app = express();
-const PORT = config.get("server");
+const PORT = config.get("serverPort");
 
 const onStart = () => {
   console.log(`Server started on port ${PORT}`);
 };
 
-const start = () => {
+const start = async () => {
   try {
+    await mongoose.connect(config.get("dbUrl"));
+
     app.listen(PORT, onStart);
-  } catch (e) {}
+  } catch (e) {
+    console.error(e);
+  }
 };
 
-start();
+start().then();
